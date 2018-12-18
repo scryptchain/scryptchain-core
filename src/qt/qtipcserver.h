057963966cc50b075784128bd4c4a33c270d2504
scryptchain <scryptchain@gmail.com>
2018-12-18 16:43:13 +0530

    Scryptchain

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ScryptChain-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ScryptChainURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
